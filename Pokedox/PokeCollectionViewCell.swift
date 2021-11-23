2402b53d3b4405ac73fa844285a7f9da57d18551
Ece Ucak <62396554+eceucak@users.noreply.github.com>
2021-11-23 20:44:07 +0300

    first commit

---
+++ Pokedox/PokeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PokeCollectionViewCell.swift
+//  Pokedox
+//
+//  Created by Ece Ucak on 21.11.2021.
+//
+
+import UIKit
+
+class PokeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Pokedox/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  Pokedox
+//
+//  Created by Ece Ucak on 21.11.2021.
+//
+
+import Foundation
